5d00c4ac62a29a0b228be0de89fc981094751e09
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2023-11-03 15:49:57 +0000

    component imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
+import Signin from './components/Signin/Signin';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Signin />
     </div>
   );
 }
