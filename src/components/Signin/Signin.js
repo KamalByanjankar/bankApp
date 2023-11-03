a3ba0d3c77b72f63b013c77a2b3dc8393effab02
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2023-11-03 15:48:42 +0000

    sign in component added

---
+++ src/components/Signin/Signin.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import "./Signin.css"
+
+function Signin() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    
+    setUsername("")
+    setPassword("")
+  }
+
+  return (
+    <div className="signin">
+      <form className="sigin__form" onSubmit={handleFormSubmit}>
+        <h3>Sign In</h3>
+        <label>
+          Username
+          <input type="email" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        </label>
+        <input type="submit" value="Log in" disabled={!username || !password} className="signin__formbtn"/>
+      </form> 
+    </div>
+  )
+}
+
+export default Signin
