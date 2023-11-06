98969f41a3fc0ee6f304c14391e6b49b65188967
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2023-11-06 11:33:47 +0000

    changes in component and css

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import "./Signin.css"
+import { Link } from 'react-router-dom'
 
 function Signin() {
   const [username, setUsername] = useState("")
@@ -7,14 +8,14 @@ function Signin() {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    
+
     setUsername("")
     setPassword("")
   }
 
   return (
     <div className="signin">
-      <form className="sigin__form" onSubmit={handleFormSubmit}>
+     <form className="sigin__form" onSubmit={handleFormSubmit}>
         <h3>Sign In</h3>
         <label>
           Username
@@ -25,7 +26,8 @@ function Signin() {
           <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
         </label>
         <input type="submit" value="Log in" disabled={!username || !password} className="signin__formbtn"/>
-      </form> 
+        <p>Don't have an account! <Link to="/signup">Register</Link></p>
+      </form>       
     </div>
   )
 }
