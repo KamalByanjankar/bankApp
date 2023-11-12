8c556d6bbd24b3cb75b0a34d0236e61ae64fe529
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2023-11-12 11:38:01 +0000

    routing implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from "./components/Header/Header";
 import Signin from './components/Signin/Signin';
-
+import Signup from './components/Signup/Signup';
 function App() {
   return (
     <div className="App">
       <Header />
-      <Signin />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Signin />} />
+          <Route path="/signup" element={<Signup/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
