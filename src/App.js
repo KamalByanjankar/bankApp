84d7aa501d5063a6d47bdf2adaf3b834e43c1d2a
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2023-11-19 19:00:46 +0000

    dashboard component protected

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Signin from './components/Signin/Signin';
 import Signup from './components/Signup/Signup';
+import Dashboard from './components/Dashboard/Dashboard';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,12 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Signin />} />
           <Route path="/signup" element={<Signup/>} />
+          <Route path="/dashboard" element={
+            <PrivateRoute>
+              <Dashboard/> 
+            </PrivateRoute>
+            }
+          />
         </Routes>
       </Router>
     </div>
