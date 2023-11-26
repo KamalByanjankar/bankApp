7796a7e17e01d8620b8a69d3ab1d921678b1bc58
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2023-11-26 13:13:12 +0000

    dashboard component edited

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,26 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './Dashboard.css'
-import { signOut } from 'firebase/auth'
-import { auth } from '../../firebase'
-import { useNavigate } from 'react-router-dom';
+import SubHeader from '../SubHeader/SubHeader'
 
 function Dashboard() {
-    const navigate = useNavigate()
-
-    const logoutBtnHandler = () => {
-        signOut(auth).then(() => {
-            localStorage.removeItem("authenticated")
-            navigate("/")
-            console.log("User Signed out successfully!")
-        }).catch((error) => {
-            console.log("Problem occured during log out!")
-        })
-    }
 
     return (
         <div className="dashboard">
-            <p>Welcome to the Bank!</p>
-            <button onClick={logoutBtnHandler}>Log out</button>
+            <SubHeader />
+            <div className="dashboard__content">
+                <p>Dashboard</p>
+            </div>
         </div>
     )
 }
