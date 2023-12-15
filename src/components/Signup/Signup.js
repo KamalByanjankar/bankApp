f433d3453bd443f0faa76964a408bf310ff9d3bc
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2023-12-15 17:51:53 +0000

    drag and drop for file upload added

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,6 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "./Signup.css"
 import { Link, useNavigate } from 'react-router-dom'
-import db, { auth } from '../../context/firebase'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { addDoc, collection } from 'firebase/firestore'
 
 function Signup() {
   const [user, setUser] = useState({
@@ -22,41 +19,30 @@ function Signup() {
     photoUrl: "",
     accountType: "",
     occupation: "",
-    monthlyIncome: "",
+    monthlyIncome: ""
   })
 
+  const refValue = useRef()
   let navigate = useNavigate()
 
-  // const handleRegistration = async (e) => {
-  //   e.preventDefault();
-  //   if(password === confirmPassword){
-  //     //Create an account 
-  //     createUserWithEmailAndPassword(auth, email, password).then(
-  //       async (userCredentials) => {
-  //         // console.log(userCredentials)
-  //         try{
-  //           const docRef = collection(db, "users")
-  //           await addDoc(docRef, {
-  //             firstName: firstName,
-  //             lastName: lastName,
-  //             email: email,
-  //             phoneNumber: phoneNumber,
-  //             dateOfBirth: dateOfBirth,
-  //             password: password,
-  //             userId: `${userCredentials.user.uid}`
-  //           })
-  //           navigate("/")
-  //           alert("User create successfully")
-  //         }catch(error){
-  //           alert("Something went wrong!")
-  //         }
-  //       }
-  //     )
-  //   }
-  //   else{
-  //     alert("Password don't match!")
-  //   }
-  // }
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  }
+
+  const handleDrop = (e) => {
+    e.preventDefault()
+    const file = e.dataTransfer.files[0]
+    if(file){
+      setUser({photoUrl: URL.createObjectURL(file)})
+    }
+  }
+
+  const onFileChange = (e) => {
+    const file = e.target.files[0]
+    if(file){
+      setUser({photoUrl: URL.createObjectURL(file)})
+    }
+  }
 
   return (
     <div className="signup">
@@ -68,8 +54,8 @@ function Signup() {
 
         <div>
           <label className="form__label">Full Name <span>*</span></label>
-          <input className="form__input" type="text" name="title" value={user.title} onChange={(e) => setUser({title: e.target.value})}/>
-          <input className="form__input" type="text" name="firstName" placeholder="First Name"/>
+          <input className="form__input" type="text" name="title" />
+          <input className="form__input" type="text" name="firstName" placeholder="First Name" onChange={(e) => setUser({firstName: e.target.value})}/>
           <input className="form__input" type="text" name="lastName" placeholder="Last Name"/>
         </div>
 
@@ -111,7 +97,15 @@ function Signup() {
 
         <div>
           <label className="form__label">Upload a photo <span>*</span></label>
-          <input className="form__input" type="file" name="photo"/>
+          <div className="dropzone" onDragOver={handleDragOver} onDrop={handleDrop} onClick={() => refValue.current.click()}>
+            <p>Drag and Drop Files to Upload</p>
+            <p>Or</p>
+            <p>Click to upload image</p>
+            <input type="file" name="photoUrl" ref={refValue} onChange={onFileChange} hidden/>
+          </div>
+          {
+            user.photoUrl ? (<img src={user.photoUrl} alt="User Profile" width={120} height={150}/>) : ""
+          }
         </div>
 
         <div>
