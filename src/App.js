36116943a4da8787327865d2e679c9fc23561dff
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2023-12-23 11:11:20 +0000

    components imported

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from "./components/Header/Header";
+import Signin from './components/Signin/Signin';
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Signin />} />
+          <Route path="/signup" element={<Signup/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
