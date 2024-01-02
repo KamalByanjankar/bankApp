43bbda3189212d2e962a3ab8e9a1c8c6f7a14298
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2024-01-02 18:55:30 +0000

    sub header component added

---
+++ src/components/SubHeader/SubHeader.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import "./SubHeader.css"
+import Logout from "../Logout/Logout"
+import { NavLink } from 'react-router-dom'
+import { useUserContext } from '../../context/UserProvider'
+import { auth } from '../../context/firebase'
+
+function SubHeader() {
+    const {userData} = useUserContext()
+
+    const lastLogin = () => {
+        let dateString = auth.currentUser.metadata.lastSignInTime;
+        dateString = dateString.split(' ').slice(0, 5).join(' ');
+        return (dateString);
+    }
+
+  return (
+    <div className="subHeader">
+        {
+            auth.currentUser !== null ? (
+                <>
+                    <div className="subHeader__content">
+                        <div>
+                            <p>Welcome, <span className="subHeader__username">{userData.data.firstName} {userData.data.lastName}</span></p>
+                            <p className="login__info">Last login: {lastLogin()}</p>
+                        </div>
+                        <div>
+                            <Logout />
+                        </div>
+                    </div>
+                    <nav className="navbar__navigation">
+                        <ul>
+                            <li>
+                                <NavLink to="/dashboard" className={({isActive}) => (isActive ? "active__style" : null)}>Dashboard</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/transfer" className={({isActive}) => (isActive ? "active__style" : null)}>Transfer</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/transactions" className={({isActive}) => (isActive ? "active__style" : null)}>Transactions</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="#">Setting</NavLink>
+                                <div className="dropdown__content">
+                                    <NavLink to="/profile" className={({isActive}) => (isActive ? "active__style" : null)}>Profile</NavLink>
+                                    <NavLink to="/card-information" className={({isActive}) => (isActive ? "active__style" : null)}>Card Information</NavLink>
+                                    <NavLink><Logout/></NavLink>
+                                </div>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="subHeader__empty"></div>       
+                </>
+            ) : (
+                window.location = "/"
+            )
+        }
+    </div>
+  )
+}
+
+export default SubHeader
