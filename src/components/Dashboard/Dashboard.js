f707d7d984f7a174fac52e5c0ab695b38dac9568
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2024-01-04 13:25:05 +0000

    dashboard component added

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Dashboard.css'
+import SubHeader from '../SubHeader/SubHeader'
+
+function Dashboard() {
+    return (
+        <div className="dashboard">
+            <SubHeader />
+        </div>
+    )
+}
+
+export default Dashboard
