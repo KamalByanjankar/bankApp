6eef69eb2dcb485fa78211a8b25a7e1261fd4785
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2024-01-05 12:13:58 +0000

    components imported and css added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Dashboard.css'
 import SubHeader from '../SubHeader/SubHeader'
+import Services from '../Services/Services'
+import AccountBalance from '../AccountBalance/AccountBalance'
+import { useUserContext } from '../../context/UserProvider'
 
 function Dashboard() {
+    const { userData } = useUserContext()
+
     return (
         <div className="dashboard">
-            <SubHeader />
+            {
+                userData.length !== 0 && (  
+                    <>
+                        <SubHeader />
+                        <div className="dashboard__content">
+                            <AccountBalance />
+                            <Services />
+                        </div>
+                    </>
+                )
+            }
         </div>
     )
 }
