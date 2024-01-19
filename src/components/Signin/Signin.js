91b4a47ebd4dae5786e8e43e18f85363abe32d79
kamalbyanjankar@gmail.com <kamalbyanjankar@gmail.com>
2024-01-19 13:09:10 +0000

    changes in form

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Signin.css"
 import { Link, useNavigate } from 'react-router-dom'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../../context/firebase'
 import { useFormContext } from '../../context/FormProvider'
 import { useUserContext } from '../../context/UserProvider'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 function Signin() {
   const { user, handleChange } = useFormContext()
   const {fetchData} = useUserContext()
   const navigate = useNavigate()
+  const [ showPassword, setShowPassword ] = useState(false)
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -29,21 +32,39 @@ function Signin() {
     })
   }
 
+  const handleShowPassword = () => {
+    setShowPassword(prev => !prev)
+  }
+
   return (
     <div className="signin">
-     <form className="sigin__form" onSubmit={handleLogin} >
+     <form className="signin__form" onSubmit={handleLogin} >
         <h3>Sign In</h3>
-        <label>
-          Username
+        <div className="signin__form__contents">
+          <label>Username</label>
           <input type="email" name="email" onChange={handleChange}/>
-        </label>
-        <label>
-          Password
-          <input type="password" name="password" onChange={handleChange}/>
-        </label>
+        </div>
+
+        <div className="signin__form__contents">
+          <label>Password</label>      
+          <input type={showPassword ? "text" : "password"} name="password" onChange={handleChange}/>
+          <span className="fa__icons" onClick={handleShowPassword}>
+            {
+              showPassword ? (
+                <FontAwesomeIcon icon={faEyeSlash} />
+                ) : (
+                  <FontAwesomeIcon icon={faEye} />
+                  )
+                }
+          </span>
+        </div>
+        
         <div className="signin__formbtn__contents">
           <input type="submit" value="Log in" disabled={!user.email || !user.password} className="signin__formbtn"/>
+        </div>
+        <div className="signin__form__footer">
           <p>Don't have an account! <Link to="/signup">Register</Link></p>
+          {/* <p>Forgot Password? Reset your password</p> */}
         </div>
       </form>       
     </div>
